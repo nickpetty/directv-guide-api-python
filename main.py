fc769caa2b69add6f5d04c460baca8371f923c7d
Nicholas Petty <nick@ihackeverything.com>
2015-01-26 15:45:53 -0600

    Start time display

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import urllib
 import urllib2
 import json
+from datetime import datetime
+from dateutil import tz
 
 url = 'http://www.directv.com/entertainment/data/guideScheduleSegment.json.jsp'
-values = {'numchannels':'20', 'channelnum':'1'}
+values = {'numchannels':'1', 'channelnum':'500'}
 
 data = urllib.urlencode(values)
 req = urllib2.Request(url, data)
@@ -13,11 +15,18 @@ page = response.read()
 #print page
 guideData = json.loads(page)
 
+tempChNum = 0
 for x in range(0,len(guideData['channels'])):
-	if 'chHd' in guideData['channels'][x]:
-		chNum = guideData['channels'][x]['chNum']
+	if guideData['channels'][x]['chNum'] != tempChNum:
+		tempChNum = chNum = guideData['channels'][x]['chNum']
 		chCall = guideData['channels'][x]['chCall']
 		swTitle = guideData['channels'][x]['schedules'][0]['prTitle']
-		print str(chNum) + ': ' + chCall + ' - ' + swTitle
+		upNext = guideData['channels'][x]['schedules'][1]['prTitle']
+		startTime = datetime.strptime(guideData['channels'][x]['schedules'][0]['prAir'][:16], '%Y-%m-%dT%H:%M').replace(tzinfo=tz.tzutc()).astimezone(tz.tzlocal()).strftime('%I:%M%p')
+		print str(chNum) + ': ' + chCall + ' - (' + startTime + ') ' + swTitle #+ ' -- Up Next: ' + upNext
 
-#print guideData['channels'][1]
+#print '2015-01-26T20:00:00.000+0000'[:16]
+# showTime = datetime.strptime('2015-01-26T20:00:00.000+0000'[:16], '%Y-%m-%dT%H:%M')
+
+# print showTime.time().strftime('%I:%M%p')
+#datatime.striptime(guideData['channels'][0]['schedules'][0]['prAir'], 
